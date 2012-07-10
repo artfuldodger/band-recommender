d9ba980e52cf9b6b56204d62c295a582256a4bfe
Jon Evans <jle@foraker.com>
2012-07-09 22:49:25 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ForakerBandCamp::Application.config.secret_token = '0188fcb51e12dc275746f22e05856e4112f4638125c5a5d59d3e602b36f3b40b608d990dd85d16029c7ad4a130b439ff7bb27bb4f84a4424f9f36a45ecf14460'
