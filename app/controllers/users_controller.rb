61360d3e46b7e12a87bbdf97984ab249714f238c
Jon Evans <jle@foraker.com>
2012-07-10 22:20:40 -0600

    Add users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,37 @@
+class UsersController < ApplicationController
+  before_filter :get_user, only: [:show, :edit, :update]
+
+  def index
+    @users = User.order(:name).paginate(page: params[:page])
+  end
+
+  def show; end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to user_path(@user), notice: 'User created!'
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @user.update_attributes(params[:user])
+      redirect_to user_path(@user), notice: 'User updated!'
+    else
+      render :edit
+    end
+  end
+
+  private
+  def get_user
+    @user = User.find(params[:id])
+  end
+end
---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,106 @@
+require 'spec_helper'
+
+describe UsersController do
+  let (:users) { [FactoryGirl.create(:user), FactoryGirl.create(:user)] }
+  let (:user) { users.first }
+
+  describe "GET 'index'" do
+    it "assigns to @users" do
+      get 'index'
+      assigns(:users).should =~ users
+    end
+
+    it 'orders them by name' do
+      User.should_receive(:order).with(:name).and_return(User)
+      get 'index'
+    end
+
+    it 'paginates the list of users' do
+      User.stub(order: User)
+      User.should_receive(:paginate)
+      get 'index'
+    end
+  end
+
+  describe "GET 'show'" do
+    it "assigns to @user" do
+      get 'show', id: user.id
+      assigns(:user).should == user
+    end
+  end
+
+  describe "GET 'new'" do
+    it "creates a new User" do
+      get 'new'
+      assigns(:user).should be_a_new User
+    end
+  end
+
+  describe "POST 'create'" do
+    context 'the user saves successfully' do
+      before do
+        User.any_instance.stub(save: true)
+        User.stub(new: user)
+      end
+
+      it 'redirects to the user show page' do
+        post 'create', user: {}
+        response.should redirect_to user_url(user)
+      end
+
+      it 'sets the flash' do
+        post 'create', user: {}
+        flash[:notice].should == 'User created!'
+      end
+    end
+
+    context 'the user fails to save' do
+      before do
+        User.any_instance.stub(save: false)
+      end
+
+      it 'renders the new template' do
+        post 'create', user: {}
+        response.should render_template :new
+      end
+    end
+  end
+
+  describe "GET 'edit'" do
+    it "assigns the requested user to @user" do
+      get 'edit', id: user.id
+      assigns(:user).should == user
+    end
+  end
+
+  describe "PUT 'update'" do
+    context 'the user updates successfully' do
+      before do
+        User.any_instance.stub(update_attributes: true)
+        User.stub(new: user)
+      end
+
+      it 'redirects to the user show page' do
+        put 'update', id: user, user: {}
+        response.should redirect_to user_url(user)
+      end
+
+      it 'sets the flash' do
+        put 'update', id: user, user: {}
+        flash[:notice].should == 'User updated!'
+      end
+    end
+
+    context 'the user fails to save' do
+      before do
+        User.any_instance.stub(update_attributes: false)
+      end
+
+      it 'renders the edit template' do
+        put 'update', id: user, user: {}
+        response.should render_template :edit
+      end
+    end
+  end
+
+end
