88b91ca6aabfaf759d6f1a8199caf0a9bd064de5
Jon Evans <jle@foraker.com>
2012-07-10 22:20:03 -0600

    Add thumb/medium styles for user picture

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
   attr_accessible :name, :username
   validates :name, :username, presence: true
   has_many :ratings
-  has_attached_file :picture
+  has_attached_file :picture, styles: { medium: "300x300>", thumb: "100x100>" }
 end
