974ae7ba311d8fed23a140f5b56c5e62c2ed02c8
Jon Evans <jle@foraker.com>
2012-07-11 22:06:36 -0600

    Seed Foraker staff

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+users = %W{
+  ben
+  bryan
+  derek
+  hunter
+  jake
+  jen
+  jon
+  jonm
+  kyle
+  neal
+  peter
+  sarah
+  sheehan
+  stirling
+  tania
+  tom
+}
+
+users.each do |user|
+  file_path = File.join(Rails.root, "db/seed_pictures", "#{user}.jpg")
+  User.create(username: user, name: user.capitalize, password: user, picture: File.open(file_path))
+end
