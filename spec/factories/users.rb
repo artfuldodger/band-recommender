d8c319a6f1be4e04e4d1d8aee427a1723e61a493
Jon Evans <jle@foraker.com>
2012-07-12 09:23:09 -0600

    Fix user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,6 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
 FactoryGirl.define do
   factory :user do
-    sequence(:username) { |n| "user{n}" }
+    sequence(:username) { |n| "username#{n}" }
     name "MyString"
     password "password"
     password_digest "encrypted_password"
