ba51f82088f15c9af549cfaf1b64dd69f7c11a01
Jon Evans <jle@foraker.com>
2012-07-16 00:17:09 -0600

    Add bands association to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ActiveRecord::Base
   validates_presence_of :password, on: :create
   validates :name, :username, presence: true
   validates :username, uniqueness: true
-  has_many :ratings
+  has_many :ratings, include: :band
+  has_many :bands, through: :ratings
   has_attached_file :picture, styles: { medium: "300x300>", thumb: "100x100>" }
 end
